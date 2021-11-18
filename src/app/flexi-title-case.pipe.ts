c49f93f8998e47790d1e42b34854365c61fec521
chrisiregbu <ciregbu@gmail.com>
2021-11-18 18:32:41 +0000

    Adding missing project folders

---
+++ src/app/flexi-title-case.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'flexititlecase'
+})
+export class FlexiTitleCasePipe implements PipeTransform {
+
+  transform(value: string, upperOrLower: string): string {
+
+    if (upperOrLower !== 'upper' && upperOrLower !== 'lower') {
+      return value;
+    }
+
+    const words: string[] = value.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      if (upperOrLower === 'upper') {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+      }
+      else {
+        words[i] = words[i].charAt(0).toLowerCase() + words[i].slice(1).toUpperCase();
+      }
+    }
+    return words.join(' ');
+  }
+}
